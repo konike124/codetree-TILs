9efc81174834fabff09ec5e465f647ad1430863e
konike124 <163543900+konike124@users.noreply.github.com>
2024-03-15 22:58:15 +0900

    Title: 실수 입력받아 계산, Time: 198ms, Memory: 14MB, Status: Passed - CodeTree

--- 240315/실수 입력받아 계산/calculation-by-inputting-a-real-number.js
+++ 240315/실수 입력받아 계산/calculation-by-inputting-a-real-number.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 let input = fs.readFileSync(0).toString().split("\n");
-a = Number(input[0]);
-b = Number(input[1]);
-console.log((a+b).toFixed(2));
+let a = Number(input[0]);
+let b = Number(input[1]);
+let result = a + b;
+
+console.log(result.toFixed(2));
