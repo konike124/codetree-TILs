93aba59d0b0ac95a3258d4be1728fee59bfb12f0
konike124 <163543900+konike124@users.noreply.github.com>
2024-03-16 21:51:30 +0900

    Title: 실수와 문자 받아 출력하기, Time: 230ms, Memory: 15MB, Status: Passed - CodeTree

--- 240316/실수와 문자 받아 출력하기/print-number--and-letter.js
+++ 240316/실수와 문자 받아 출력하기/print-number--and-letter.js
@@ -2,4 +2,6 @@ const fs = require('fs');
 let a = fs.readFileSync(0).toString().trim().split("\n");
 let b = Number(a[1]);
 let c = Number(a[2]);
-console.log(`${a[0]}\n${b.toFixed(2)}\n${c.toFixed(2)}`);
+console.log(`${a[0]}
+${b.toFixed(2)}
+${c.toFixed(2)}`);
