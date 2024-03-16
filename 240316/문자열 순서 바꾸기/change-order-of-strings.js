ccd7340c00acf7808aa69591208003e74ef95a94
konike124 <163543900+konike124@users.noreply.github.com>
2024-03-16 21:57:02 +0900

    Title: 문자열 순서 바꾸기, Time: 164ms, Memory: 14MB, Status: Passed - CodeTree

--- 240316/문자열 순서 바꾸기/change-order-of-strings.js
+++ 240316/문자열 순서 바꾸기/change-order-of-strings.js
@@ -1,4 +1,11 @@
 const fs = require('fs');
-let a = fs.readFileSync(0).toString().trim().split("\n");
-console.log(a[1]);
-console.log(a[0]);
+let input = fs.readFileSync(0).toString().trim().split("\n");
+let s = input[0];
+let t = input[1];
+
+// 값 교체
+[s, t] = [t, s]
+
+//출력
+console.log(s)
+console.log(t)
