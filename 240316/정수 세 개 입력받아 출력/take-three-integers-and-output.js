9c7a7ad41cbbda49b90564e07232aa629bb2a68e
konike124 <163543900+konike124@users.noreply.github.com>
2024-03-16 22:06:22 +0900

    Title: 정수 세 개 입력받아 출력, Time: 240ms, Memory: 15MB, Status: Passed - CodeTree

--- 240316/정수 세 개 입력받아 출력/take-three-integers-and-output.js
+++ 240316/정수 세 개 입력받아 출력/take-three-integers-and-output.js
@@ -1,6 +1,4 @@
 const fs = require('fs');
-let input = fs.readFileSync(0).toString().split("\n");
-let [ab, c] = input;
-let [a, b] = ab.trim().split(" ").map(Number);
-let cNum = Number(c);
-console.log(a, b, cNum);
+let input = fs.readFileSync(0).toString().trim().split("\n");
+
+console.log(input[0],input[1]);
