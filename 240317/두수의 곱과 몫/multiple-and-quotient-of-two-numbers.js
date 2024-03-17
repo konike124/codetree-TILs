7fc59e01cac077473f0cc83d0a4516d336938d3e
konike124 <163543900+konike124@users.noreply.github.com>
2024-03-17 18:11:12 +0900

    Title: 두수의 곱과 몫, Time: 200ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240317/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.js
+++ 240317/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.js
@@ -2,6 +2,6 @@ const fs = require('fs');
 let e =fs.readFileSync(0).toString().trim().split(" ")
 let e1 = Number(e[0]);
 let e2 = Number(e[1]);
-
+let result = e1/e2
 console.log(`${e1} * ${e2} = ${e1*e2}`)
-console.log(`${e1} / ${e2} = ${e1/e2.toFixed(0)}`)
+console.log(`${e1} / ${e2} = ${result}`)
