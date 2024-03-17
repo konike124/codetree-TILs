a29fbd24713d5591d1346879b9e02c8ea4e870de
konike124 <163543900+konike124@users.noreply.github.com>
2024-03-17 18:30:35 +0900

    Title: 2개의 정수를 서로 더하기, Time: 180ms, Memory: 14MB, Status: Passed - CodeTree

--- 240317/2개의 정수를 서로 더하기/add-two-integers-each-other.js
+++ 240317/2개의 정수를 서로 더하기/add-two-integers-each-other.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 let e =fs.readFileSync(0).toString().trim().split(" ")
-let e1 = Number(e[0]);
-let e2 = Number(e[1]);
-let a = (e1+e2); // a+=87;
-let b = (e2+a); // b%=10;
-console.log(`${a} ${b}`)
+let a = Number(e[0]);
+let b = Number(e[1]);
+a+=b;
+b+=a;
+console.log(a,b)
