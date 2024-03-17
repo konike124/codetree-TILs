865ab96e27ccbe1dcd8611050b4c4048e19bef5c
konike124 <163543900+konike124@users.noreply.github.com>
2024-03-18 05:24:25 +0900

    Title: 체질량지수, Time: 168ms, Memory: 14MB, Status: Passed - CodeTree

--- 240318/체질량지수/bmi.js
+++ 240318/체질량지수/bmi.js
@@ -3,7 +3,7 @@ let input = fs.readFileSync(0).toString().trim().split(" ");
 let height = Number(input[0])/100;
 let kg = Number(input[1]);
 let bmi = (kg / (height**2))
-console.log(parseInt(bmi))
 
+console.log(parseInt(bmi))
 if (bmi>=25) {
     console.log("Obesity")}
