85f64e79012d10d8d5adfbf3914cca3663a6f04b
konike124 <163543900+konike124@users.noreply.github.com>
2024-03-17 18:22:42 +0900

    Title: 입력받아 연산하기, Time: 188ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240317/입력받아 연산하기/take-input-and-operate.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+let e =fs.readFileSync(0).toString().trim().split("\n")
+let e1 = Number(e[0]);
+let e2 = Number(e[1]);
+let a = (e1+87);
+let b = (e2%10);
+console.log(`${a}
+${b}`)
