381dd0e45120785dc13c280b89ec9479662aa8c4
konike124 <163543900+konike124@users.noreply.github.com>
2024-03-18 17:42:15 +0900

    Title: 2개 중 최대, Time: 188ms, Memory: 14MB, Status: Passed - CodeTree

--- 240318/2개 중 최대/max-of-two-nums.js
+++ 240318/2개 중 최대/max-of-two-nums.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split(" ");
-let [a,b] = [(input[0],input[1])]
-
-a = a < b ? console.log(b) : console.log(a);
+let a = Number(input[0])
+let b = Number(input[1])
+let n
+n = (a < b) ? console.log(b) : console.log(a);
