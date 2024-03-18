60cb4b13ee1734bb5bbdc4ebb0597480b67ab73d
konike124 <163543900+konike124@users.noreply.github.com>
2024-03-18 18:07:10 +0900

    Title: 물의 상태, Time: 175ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240318/물의 상태/state-of-water.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+let water = Number(fs.readFileSync(0).toString().trim());
+
+if (water < 0) console.log('ice')
+
+else if (water >= 100) console.log('vapor')
+
+else console.log(water)
