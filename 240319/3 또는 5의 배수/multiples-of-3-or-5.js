566593f083a8c8a79291ae17f3e62e295d968ccf
konike124 <163543900+konike124@users.noreply.github.com>
2024-03-19 20:51:49 +0900

    Title: 3 또는 5의 배수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240319/3 또는 5의 배수/multiples-of-3-or-5.js
@@ -0,0 +1,17 @@
+let fs = require("fs");
+let a = Number(fs.readFileSync(0).toString().trim().split(" "));
+
+
+if (a % 3 === 0) {
+    console.log('YES')
+}
+else {
+    cosnole.log('NO')
+}
+
+if (a % 5 === 0) {
+    console.log('YES')
+}
+else {
+    cosnole.log('NO')
+}
