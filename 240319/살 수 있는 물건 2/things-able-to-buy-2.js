b88f3bf3b9133aef850ce8bec80ddf700bb67459
konike124 <163543900+konike124@users.noreply.github.com>
2024-03-19 17:33:04 +0900

    Title: 살 수 있는 물건 2, Time: 235ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240319/살 수 있는 물건 2/things-able-to-buy-2.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+let a = Number(fs.readFileSync(0).toString().trim());
+
+if (a >= 3000) {
+    console.log('book')
+}
+else if (a >= 1000) {
+     console.log('mask')
+}
+else if (a >= 500) {
+    console.log('pen')
+}
+else {
+    console.log('No')
+}
