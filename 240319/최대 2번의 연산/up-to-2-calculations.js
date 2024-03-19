62fcca8d996ad32ed4ce1841bbc660390a093129
konike124 <163543900+konike124@users.noreply.github.com>
2024-03-19 18:17:01 +0900

    Title: 최대 2번의 연산, Time: 220ms, Memory: 47MB, Status: Passed - CodeTree

--- 240319/최대 2번의 연산/up-to-2-calculations.js
+++ 240319/최대 2번의 연산/up-to-2-calculations.js
@@ -3,13 +3,10 @@ let a = Number(fs.readFileSync(0).toString().trim());
 let result
 
 if (a % 2 === 0) {
-    result = a/2
+    a/=2;
 }
-else if (a % 2 === 1) {
-    result=(a+1)/2
+if (a % 2 === 1) {
+    a+=1;
+    a/=2;
 }
-
-if (a % 2 === 0 & result % 2 === 1) {
-    result = (result+1)/2
-}
-console.log(result);
+console.log(a);
