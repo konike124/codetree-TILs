c449609eb58a97965d11e739b1d386b6bf8800c1
konike124 <163543900+konike124@users.noreply.github.com>
2024-03-20 20:52:43 +0900

    Title: 세 정수의 최솟값, Time: 186ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 240320/세 정수의 최솟값/minimum-of-three-numbers.js
@@ -0,0 +1,14 @@
+const fs=require('fs')
+let input=fs.readFileSync(0).toString().trim().split(" ")
+
+let a=Number(input[0]), b=Number(input[1]), c=Number(input[2])
+
+if (a<=b && a<=c) {
+    console.log(a)
+}
+else if (b<=a && b<=c) {
+    console.log(b)
+}
+else {
+    console.log(c)
+}
