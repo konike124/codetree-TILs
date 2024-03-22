10825fab707965cfd9b4218385a3943a6620aca5
konike124 <163543900+konike124@users.noreply.github.com>
2024-03-22 20:41:38 +0900

    Title: 남녀노소 구분짓기, Time: 190ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 240322/남녀노소 구분짓기/sex-and-age.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("\n");
+
+let [a,b] = input.map(Number); // 문자열을 숫자로 변환
+
+if (a === 0) {
+    if (b >= 19) {
+        console.log("MAN");
+    } else {
+        console.log("BOY");
+    }
+} else {
+    if (b >= 19) {
+        console.log("WOMAN");
+    } else {
+        console.log("GIRL");
+    }
+}
