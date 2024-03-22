0ef4eeb66b17d50e3cd922d9185391d592d0832a
konike124 <163543900+konike124@users.noreply.github.com>
2024-03-22 23:22:52 +0900

    Title: 코로나 메뉴얼, Time: 177ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240322/코로나 메뉴얼/covid-manual.js
+++ 240322/코로나 메뉴얼/covid-manual.js
@@ -1,27 +1,31 @@
-const fs = require('fs')
-let input = fs.readFileSync(0).toString().trim().split("\n")
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().trim().split("\n");
 
-let result=""
+let result = "";
 
 let [symptom1, temperature1] = input[0].split(" ");
 let [symptom2, temperature2] = input[1].split(" ");
 let [symptom3, temperature3] = input[2].split(" ");
 
+temperature1 = Number(temperature1);
+temperature2 = Number(temperature2);
+temperature3 = Number(temperature3);
+
 if (symptom1 === "Y") {
     if (temperature1 >= 37) {
-        result='A';
+        result = "A";
     } else {
-        result='C';
-    }  
-} 
-else {
+        result = "C";
+    }
+} else { // 증상이 없는 경우
     if (temperature1 >= 37) {
-        result='B'
+        result = "B";
     } else {
-        result='D'
+        result = "D";
     }
 }
 
+// A일 경우 E 여부 확인
 if (result === "A") {
     if (symptom2 === "Y" && temperature2 >= 37) {
         result = "E";
