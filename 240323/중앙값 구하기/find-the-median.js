019e31c1cf1af947698d41de609e690ecbfae231
konike124 <163543900+konike124@users.noreply.github.com>
2024-03-23 04:58:11 +0900

    Title: 중앙값 구하기, Time: 185ms, Memory: 14MB, Status: Passed - CodeTree

--- 240323/중앙값 구하기/find-the-median.js
+++ 240323/중앙값 구하기/find-the-median.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
 let input = fs.readFileSync(0).toString().trim().split(" ");
 
-let [a,b,c] = input
+let [a,b,c] =input.map(Number)
 
 if ((a<=b && a>=c)||(a<=c && a>=b)) {
     console.log(a)
 }
-else if ((a>=b && b>=c) || (c>=b && b>=a)) {
+else if ((a>=b && b>=c)||(c>=b && b>=a)) {
     console.log(b)
 }
 else {
