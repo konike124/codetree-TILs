efc9c28b9998e95180beeaf3a8030ca3db89462b
konike124 <163543900+konike124@users.noreply.github.com>
2024-03-22 20:21:43 +0900

    Title: 두 사람, Time: 175ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240322/두 사람/two-person.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("\n");
+
+let a =input[0].split(" ")
+let b =input[1].split(" ")
+
+let a_age=a[0], a_sex=a[1]
+let b_age=b[0], b_sex=b[1]
+
+if ((a_age||b_age) >=19 && (a_sex||b_sex)=='M') {
+    console.log(1)
+} 
+else console.log(0)
