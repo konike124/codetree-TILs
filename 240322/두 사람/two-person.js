164c045ad784687df6109f8160744a19d55735be
konike124 <163543900+konike124@users.noreply.github.com>
2024-03-22 20:24:26 +0900

    Title: 두 사람, Time: 208ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240322/두 사람/two-person.js
+++ 240322/두 사람/two-person.js
@@ -7,7 +7,7 @@ let b =input[1].split(" ")
 let a_age=a[0], a_sex=a[1]
 let b_age=b[0], b_sex=b[1]
 
-if ((a_age||b_age) >=19 && (a_sex||b_sex)=='M') {
+if ((a_age||b_age) >=19 && (a_sex||b_sex)==='M') {
     console.log(1)
-} 
+}
 else console.log(0)
