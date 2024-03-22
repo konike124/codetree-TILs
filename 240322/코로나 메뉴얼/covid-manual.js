e49a480e4928dd9a3ef01c936c20c3e4a04dedf0
konike124 <163543900+konike124@users.noreply.github.com>
2024-03-22 23:37:53 +0900

    Title: 코로나 메뉴얼, Time: 180ms, Memory: 14MB, Status: Passed - CodeTree

--- 240322/코로나 메뉴얼/covid-manual.js
+++ 240322/코로나 메뉴얼/covid-manual.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 let input = fs.readFileSync(0).toString().trim().split("\n");
 
-let result = "";
+let result = "N"; // 초기값을 N으로 설정
 
 let [symptom1, temperature1] = input[0].split(" ");
 let [symptom2, temperature2] = input[1].split(" ");
@@ -11,12 +11,22 @@ temperature1 = Number(temperature1);
 temperature2 = Number(temperature2);
 temperature3 = Number(temperature3);
 
-if ((symptom1 === "Y" && temperature1 >= 37) || (symptom2 === "Y" && temperature2 >= 37) || (symptom3 === "Y" && temperature3 >= 37)) {
-    result = "E";
-} else if (symptom1 === "Y" || symptom2 === "Y" || symptom3 === "Y") {
-    result = "C";
-} else {
-    result = "N";
+let countA = 0;
+
+// 각 사람의 상태를 확인하고, A가 2명 이상이면 E를 결과로 설정
+if (symptom1 === "Y" && temperature1 >= 37) {
+    countA++;
 }
+if (symptom2 === "Y" && temperature2 >= 37) {
+    countA++;
+}
+if (symptom3 === "Y" && temperature3 >= 37) {
+    countA++;
+}
+
+if (countA >= 2) {
+    result = "E";
+}
+else result='N'
 
 console.log(result);
