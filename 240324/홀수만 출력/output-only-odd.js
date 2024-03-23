7cca44187971637203c500e2b248da9318d9266d
konike124 <163543900+konike124@users.noreply.github.com>
2024-03-24 01:27:56 +0900

    Title: 홀수만 출력, Time: 186ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240324/홀수만 출력/output-only-odd.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+let [a, b] = input.map(Number);
+let result = "";
+
+for (let i = a; i <= b; i+=2) {
+    result += (i) + " ";
+}
+console.log(result);
