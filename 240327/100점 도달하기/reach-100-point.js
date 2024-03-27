3693e6aab4b31697b8c5f8117f57ebacb13344e8
konike124 <163543900+konike124@users.noreply.github.com>
2024-03-27 18:23:13 +0900

    Title: 100점 도달하기, Time: 204ms, Memory: 47MB, Status: Wrong Answer - CodeTree

---
+++ 240327/100점 도달하기/reach-100-point.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+let n = fs.readFileSync(0).toString().trim();
+let result=""
+
+for (let i=n; i<=100; i++) {
+    if (i>=90) {
+        result+='A'+" "
+    }
+    else if (i>=80) {
+        result+='B'+" "
+    }
+    else if (i>=70) {
+        result+='c'+" "    
+    }
+    else if (i>=60) {
+        result+='D'+" "
+    }
+    else result+='F'
+}
+console.log(result)
