31d9a578b7f576e8a09a9f9a0b0565d44b573583
konike124 <163543900+konike124@users.noreply.github.com>
2024-03-27 18:59:09 +0900

    Title: a 부터 b 까지, Time: 201ms, Memory: 20MB, Status: Wrong Answer - CodeTree

--- 240327/a 부터 b 까지/a-to-b.js
+++ 240327/a 부터 b 까지/a-to-b.js
@@ -3,14 +3,14 @@ let input = fs.readFileSync(0).toString().trim().split(" ");
 let a = Number(input[0]), b = Number(input[1]), result=a
 
 
-for (let i=a; i<b; i) {
+for (let i=a; i<b;) {
     if (i%2===1) {
         i*=2;
+        result+=" "+i
     }
     else if (i%2===0) {
         i+=3;
+        result+=" "+i
     }
-    result+=" "+i
-    
 }
 console.log(result)
