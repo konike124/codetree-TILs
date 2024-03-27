7d1a9693697cf75b5351faab9ab2cfc7a5c4fe70
konike124 <163543900+konike124@users.noreply.github.com>
2024-03-27 19:02:44 +0900

    Title: a 부터 b 까지, Time: 195ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240327/a 부터 b 까지/a-to-b.js
+++ 240327/a 부터 b 까지/a-to-b.js
@@ -6,11 +6,15 @@ let a = Number(input[0]), b = Number(input[1]), result=a
 for (let i=a; i<b;) {
     if (i%2===1) {
         i*=2;
+        if (i<b) {
         result+=" "+i
+        }
     }
     else if (i%2===0) {
         i+=3;
+        if (i<b) {
         result+=" "+i
+        }
     }
 }
 console.log(result)
