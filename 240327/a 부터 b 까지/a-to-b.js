bbfc7d49d1c46f9481198d7d0764d9b62939aba3
konike124 <163543900+konike124@users.noreply.github.com>
2024-03-27 18:56:14 +0900

    Title: a 부터 b 까지, Time: 165ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240327/a 부터 b 까지/a-to-b.js
+++ 240327/a 부터 b 까지/a-to-b.js
@@ -1,19 +1,16 @@
 const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split(" ");
-let a = Number(input[0]),
-    b = Number(input[1]),
-    result = "",
-    ans = a; // 초기값을 a로 설정합니다.
+let a = Number(input[0]), b = Number(input[1]), result=a
 
-for (let i = a; i <= b; i++) {
-    if (ans % 2 === 1) {
-        ans *= 2;
-    } else {
-        ans += 3;
+
+for (let i=a; i<b; i) {
+    if (i%2===1) {
+        i*=2;
     }
-    result += ans + " ";
-    if (ans >= b) {
-        break; // 결과값이 b를 넘어가면 반복문을 종료합니다.
+    else if (i%2===0) {
+        i+=3;
     }
+    result+=" "+i
+    
 }
-console.log(`${a}`,result.trim());
+console.log(result)
